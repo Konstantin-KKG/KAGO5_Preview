bff7352c1c9c9d2a452e360fae59178d14813983
Remilia-1 <lampkons@kkg-do.eu>
2023-09-27 10:35:01 +0200

    Rewrote KAGO Log Messages

--- src/main/java/KAGO_framework/Config.java
+++ src/main/java/KAGO_framework/Config.java
@@ -1,16 +1,13 @@
 package KAGO_framework;
 
 /**
- * In dieser Klasse werden globale, statische Einstellungen verwaltet.
- * Diese beziehen sich nur auf die Funktionsweise des Frameworks.
- * Für individuelle Einstellungen am eigenen Projekt sollte die Config-Datei im Paket "my_project"
- * verwendet werden.
+ * Defines globally static framework settings.
  */
 public class Config {
 
     // Frameworkversion
     public final static String VERSION = "KAGO.5 - 26.09.2023";
-    public final static String JAVA_SUPPORTED = "Java 11 bis Java 15";
+    public final static String JAVA_SUPPORTED = "Java 11 - Java 15";
 
     // Schaltet die Infomeldungen des Frameworks an oder aus
     public final static boolean INFO_MESSAGES = true;
--- src/main/java/KAGO_framework/Core/Launcher.java
+++ src/main/java/KAGO_framework/Core/Launcher.java
@@ -1,47 +1,31 @@
 package KAGO_framework.Core;
 
 import KAGO_framework.Config;
-
 import java.awt.*;
 
-/**
- * Diese Klasse enthält die main-Methode. Von ihr wird als erstes ein Objekt innerhalb der main-Methode erzeugt,
- * die zu Programmstart aufgerufen wird und kein Objekt benötigt, da sie statisch ist.
- * Die erste Methode, die also nach der main-Methode aufgerufen wird, ist der Konstruktor dieser Klasse. Aus ihm
- * wird alles weitere erzeugt.
- * Vorgegebene Klasse des Frameworks. Modifikation auf eigene Gefahr.
- */
 public class Launcher {
-
-    // Attribute
-
-    // Referenzen
-
-    /**
-     * Diese Methode startet das gesamte Framework und erzeugt am Ende dieses Prozesses ein Objekt der Klasse
-     * ProgramController aus dem Paket "my_project > control"
-     */
     public static void startFramework(){
-        if ( Config.INFO_MESSAGES) System.out.println("***** PROGRAMMSTART ("+" Framework: "+Config.VERSION+") *****.");
-        if ( Config.INFO_MESSAGES) System.out.println("** Supported Java-Versions: "+ Config.JAVA_SUPPORTED);
-        if ( Config.INFO_MESSAGES) System.out.println("");
-        if ( Config.INFO_MESSAGES) System.out.println("** Ablauf der Framework-Initialisierung: **");
+        if (Config.INFO_MESSAGES) {
+            System.out.println("***** PROGRAM START (Framework: " + Config.VERSION + ") *****.");
+            System.out.println("** Supported Java-Versions: " + Config.JAVA_SUPPORTED);
+            System.out.println("** Initializing Framework: **");
+        }
+
         EventQueue.invokeLater(
                 new Runnable() {
                     @Override
                     public void run() {
-                        if ( Config.INFO_MESSAGES) System.out.println("  > Main-Methode: Programmstart. Erzeuge ein Objekt der Main-Controller-Klasse.");
+                        if ( Config.INFO_MESSAGES)
+                            System.out.println("  > KAGO-Main-Method: Program Start. Creating KAGO Launcher Instance.");
                         new Launcher();
                     }
                 });
     }
 
-    /**
-     * Der Konstruktor der Klasse-MainController ist die erste Methode, die nach der Main-Methode
-     * aufgerufen wird. Hier wird der Programmfluss geregelt.
-     */
     public Launcher(){
-        if ( Config.INFO_MESSAGES) System.out.println("  > MainController: Ich wurde erzeugt. Erstelle ein ViewController-Objekt zur Steuerung der View...");
+        if ( Config.INFO_MESSAGES)
+            System.out.println("  > KAGO-Launcher: Successfully created. Setting up Game Systems...");
+
         new GameManager();
     }
 
