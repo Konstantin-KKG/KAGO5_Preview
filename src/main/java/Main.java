18757b838399ada08469c612d42d89105f72ba52
Konstantin-KKG <lampkons@kkg-do.eu>
2023-10-03 01:01:35 +0200

    Fixed Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
         KAGO_framework.Core.Launcher.StartFramework();
     }
 }
